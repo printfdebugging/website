79d296e2fbb8417b9bd94947911730cfbce8b196
Sahil Gautam <printfdebugging@gmail.com>
2025-12-17 03:34:53 +0530

    use container appropriate paths

--- scripts/build-graphics.sh
+++ scripts/build-graphics.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-if [ -d "$HOME/repos/learnopengl" ]; then
-    GRAPHICS_PROJECT_DIR="$HOME/repos/learnopengl"
-    WEBSITE_PUBLIC_DIR="$HOME/repos/website/public"
-else
-    [ ! -d "learnopengl" ] && git clone "https://gitlab.com/printfdebugging/learnopengl.git" --depth 1
-    GRAPHICS_PROJECT_DIR="learnopengl"
-    WEBSITE_PUBLIC_DIR="public"
-fi
-
-# build and install cmake project
-(
-    emcmake cmake \
-        -DUSE_GENERATED_INDEX_HTML=TRUE \
-        -DGENERATED_GRAPHICS_DIR_PATH="${WEBSITE_PUBLIC_DIR}" \
-        -S "${GRAPHICS_PROJECT_DIR}" -B "${GRAPHICS_PROJECT_DIR}/build"
-
-    cmake --build "${GRAPHICS_PROJECT_DIR}/build"
-    cmake --install "${GRAPHICS_PROJECT_DIR}/build" --prefix "${GRAPHICS_PROJECT_DIR}/install"
+dependencies=(
+    emcmake
+    cmake
+    make
+    git
 )
+
+for dependency in "${dependencies[@]}"; do
+    if ! which "$dependency" >/dev/null 2>&1; then
+        echo "${dependency} not found" && exit 1
+    fi
+done
+
+GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
+WEBSITE_PUBLIC_DIR="/builds/printfdebugging/website/public"
+# GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
+
+# NOTE: this is already done in ./generate-graphics.sh
+# git clone --depth 1 \
+#     "$GRAPHICS_PROJECT_UPSTREAM" \
+#     "$GRAPHICS_PROJECT_DIR"
+
+emcmake cmake \
+    -DUSE_GENERATED_INDEX_HTML=TRUE \
+    -DGENERATED_GRAPHICS_DIR_PATH="${WEBSITE_PUBLIC_DIR}" \
+    -S "${GRAPHICS_PROJECT_DIR}" -B "${GRAPHICS_PROJECT_DIR}/build"
+
+cmake --build "${GRAPHICS_PROJECT_DIR}/build"
+cmake --install "${GRAPHICS_PROJECT_DIR}/build" --prefix "${GRAPHICS_PROJECT_DIR}/install"
 
 # move installed artifacts to blog directory
 cp -r ${GRAPHICS_PROJECT_DIR}/install/bin/* "${WEBSITE_PUBLIC_DIR}/graphics"
--- scripts/generate-graphics.sh
+++ scripts/generate-graphics.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [ -d "$HOME/repos/learnopengl" ]; then
-    GRAPHICS_PROJECT_DIR="$HOME/repos/learnopengl"
-else
-    [ ! -d "learnopengl" ] && {
-        git clone "https://gitlab.com/printfdebugging/learnopengl.git" --depth 1 || exit 1
-    }
-    GRAPHICS_PROJECT_DIR="learnopengl"
-fi
+dependencies=(
+    zola
+    git
+)
+
+for dependency in "${dependencies[@]}"; do
+    if ! which "$dependency" >/dev/null 2>&1; then
+        echo "${dependency} not found" && exit 1
+    fi
+done
+
+GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
+WEBSITE_DIR="/builds/printfdebugging/website"
+GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
+
+git clone --depth 1 \
+    "$GRAPHICS_PROJECT_UPSTREAM" \
+    "$GRAPHICS_PROJECT_DIR"
 
 for tutorialDir in $(find "${GRAPHICS_PROJECT_DIR}/source/tutorials/" -mindepth 1 -maxdepth 1 -type d); do
     # step 1: check if the tutorials have a readme
@@ -22,8 +32,8 @@ for tutorialDir in $(find "${GRAPHICS_PROJECT_DIR}/source/tutorials/" -mindepth 
 
     # step 3: copy the file to content/graphics
     dirName="$(basename "$tutorialDir")"
-    mkdir -p "content/graphics/${dirName}"
-    cp "${tutorialDir}/README.md" "content/graphics/${dirName}/index.md"
+    mkdir -p "${WEBSITE_DIR}/content/graphics/${dirName}"
+    cp "${tutorialDir}/README.md" "${WEBSITE_DIR}/content/graphics/${dirName}/index.md"
 done
 
 # build the blog
