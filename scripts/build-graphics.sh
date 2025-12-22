4f17a1f008f2ea74c5b7a726ed0e4cc117981a3c
Sahil Gautam <printfdebugging@gmail.com>
2025-12-22 20:58:21 +0530

    don't build graphics for now

--- scripts/build-graphics.sh
+++ scripts/build-graphics.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-dependencies=(
-    cmake
-    make
-    git
-)
-
-for dependency in "${dependencies[@]}"; do
-    if ! which "$dependency" >/dev/null 2>&1; then
-        echo "${dependency} not found" && exit 1
-    fi
-done
-
-GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
-WEBSITE_PUBLIC_DIR="/builds/printfdebugging/website/public"
-# GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
-
-# NOTE: this is already done in ./generate-graphics.sh
-# git clone --depth 1 \
-#     "$GRAPHICS_PROJECT_UPSTREAM" \
-#     "$GRAPHICS_PROJECT_DIR"
-
-source /etc/profile.d/emscripten.sh
-
-emcmake cmake \
-    -DUSE_GENERATED_INDEX_HTML=TRUE \
-    -DGENERATED_GRAPHICS_DIR_PATH="${WEBSITE_PUBLIC_DIR}/graphics" \
-    -S "${GRAPHICS_PROJECT_DIR}" -B "${GRAPHICS_PROJECT_DIR}/build"
-
-cmake --build "${GRAPHICS_PROJECT_DIR}/build"
-cmake --install "${GRAPHICS_PROJECT_DIR}/build" --prefix "${GRAPHICS_PROJECT_DIR}/install"
-
-# move installed artifacts to blog directory
-cp -r ${GRAPHICS_PROJECT_DIR}/install/bin/* "${WEBSITE_PUBLIC_DIR}/graphics"
+# dependencies=(
+#     cmake
+#     make
+#     git
+# )
+#
+# for dependency in "${dependencies[@]}"; do
+#     if ! which "$dependency" >/dev/null 2>&1; then
+#         echo "${dependency} not found" && exit 1
+#     fi
+# done
+#
+# GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
+# WEBSITE_PUBLIC_DIR="/builds/printfdebugging/website/public"
+# # GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
+#
+# # NOTE: this is already done in ./generate-graphics.sh
+# # git clone --depth 1 \
+# #     "$GRAPHICS_PROJECT_UPSTREAM" \
+# #     "$GRAPHICS_PROJECT_DIR"
+#
+# source /etc/profile.d/emscripten.sh
+#
+# emcmake cmake \
+#     -DUSE_GENERATED_INDEX_HTML=TRUE \
+#     -DGENERATED_GRAPHICS_DIR_PATH="${WEBSITE_PUBLIC_DIR}/graphics" \
+#     -S "${GRAPHICS_PROJECT_DIR}" -B "${GRAPHICS_PROJECT_DIR}/build"
+#
+# cmake --build "${GRAPHICS_PROJECT_DIR}/build"
+# cmake --install "${GRAPHICS_PROJECT_DIR}/build" --prefix "${GRAPHICS_PROJECT_DIR}/install"
+#
+# # move installed artifacts to blog directory
+# cp -r ${GRAPHICS_PROJECT_DIR}/install/bin/* "${WEBSITE_PUBLIC_DIR}/graphics"
--- scripts/generate-graphics.sh
+++ scripts/generate-graphics.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 
-dependencies=(
-    zola
-    git
-)
-
-for dependency in "${dependencies[@]}"; do
-    if ! which "$dependency" >/dev/null 2>&1; then
-        echo "${dependency} not found" && exit 1
-    fi
-done
-
-GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
-WEBSITE_DIR="/builds/printfdebugging/website"
-GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
-
-[ ! -d "$GRAPHICS_PROJECT_DIR" ] && git clone --depth 1 \
-    "$GRAPHICS_PROJECT_UPSTREAM" \
-    "$GRAPHICS_PROJECT_DIR"
-
-(
-    cd "$GRAPHICS_PROJECT_DIR"
-    git submodule init
-    git submodule update --recursive
-
-    [ -d "$GRAPHICS_PROJECT_DIR/build" ] && rm -rf "$GRAPHICS_PROJECT_DIR/install"
-    [ -d "$GRAPHICS_PROJECT_DIR/install" ] && rm -rf "$GRAPHICS_PROJECT_DIR/install"
-)
-
-for tutorialDir in $(find "${GRAPHICS_PROJECT_DIR}/source/tutorials/" -mindepth 1 -maxdepth 1 -type d); do
-    # step 1: check if the tutorials have a readme
-    [ -f "${tutorialDir}/README.md" ] || {
-        echo "${tutorialDir}/README.md not found" && exit 1
-    }
-
-    # step 2: check if the readme has the include directive
-    grep -qe "{{ include(path=\"/canvas.md\") }}" "${tutorialDir}/README.md" || {
-        echo "${tutorialDir}/README.md doesn't have include /canvas.md directive" && exit 1
-    }
-
-    # step 3: copy the file to content/graphics
-    dirName="$(basename "$tutorialDir")"
-    mkdir -p "${WEBSITE_DIR}/content/graphics/${dirName}"
-
-    echo "copying ${tutorialDir}/README.md to ${WEBSITE_DIR}/content/graphics/${dirName}/index.md"
-    cp "${tutorialDir}/README.md" "${WEBSITE_DIR}/content/graphics/${dirName}/index.md"
-done
-
-# build the blog
 zola build
+
+# dependencies=(
+#     zola
+#     git
+# )
+#
+# for dependency in "${dependencies[@]}"; do
+#     if ! which "$dependency" >/dev/null 2>&1; then
+#         echo "${dependency} not found" && exit 1
+#     fi
+# done
+#
+# GRAPHICS_PROJECT_DIR="/builds/printfdebugging/website/learnopengl"
+# WEBSITE_DIR="/builds/printfdebugging/website"
+# GRAPHICS_PROJECT_UPSTREAM="https://gitlab.com/printfdebugging/learnopengl.git"
+#
+# [ ! -d "$GRAPHICS_PROJECT_DIR" ] && git clone --depth 1 \
+#     "$GRAPHICS_PROJECT_UPSTREAM" \
+#     "$GRAPHICS_PROJECT_DIR"
+#
+# (
+#     cd "$GRAPHICS_PROJECT_DIR"
+#     git submodule init
+#     git submodule update --recursive
+#
+#     [ -d "$GRAPHICS_PROJECT_DIR/build" ] && rm -rf "$GRAPHICS_PROJECT_DIR/install"
+#     [ -d "$GRAPHICS_PROJECT_DIR/install" ] && rm -rf "$GRAPHICS_PROJECT_DIR/install"
+# )
+#
+# for tutorialDir in $(find "${GRAPHICS_PROJECT_DIR}/source/tutorials/" -mindepth 1 -maxdepth 1 -type d); do
+#     # step 1: check if the tutorials have a readme
+#     [ -f "${tutorialDir}/README.md" ] || {
+#         echo "${tutorialDir}/README.md not found" && exit 1
+#     }
+#
+#     # step 2: check if the readme has the include directive
+#     grep -qe "{{ include(path=\"/canvas.md\") }}" "${tutorialDir}/README.md" || {
+#         echo "${tutorialDir}/README.md doesn't have include /canvas.md directive" && exit 1
+#     }
+#
+#     # step 3: copy the file to content/graphics
+#     dirName="$(basename "$tutorialDir")"
+#     mkdir -p "${WEBSITE_DIR}/content/graphics/${dirName}"
+#
+#     echo "copying ${tutorialDir}/README.md to ${WEBSITE_DIR}/content/graphics/${dirName}/index.md"
+#     cp "${tutorialDir}/README.md" "${WEBSITE_DIR}/content/graphics/${dirName}/index.md"
+# done
+#
+# # build the blog
+# zola build
